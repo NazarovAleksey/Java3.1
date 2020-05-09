97644784af82e5f62839a32c786934ccbb2be06b
Alexey Nazarov <alexeyka1987@gmail.com>
2020-05-09 19:06:16 +0500

    Add Miles

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 3518;
+        int miles = amount / 20;
+
+        System.out.println("Начислено миль: " + miles);
+    }
+}
