1960f5de713453c7d03c256dc1de00894be68a43
NazarovAleksey <63412103+NazarovAleksey@users.noreply.github.com>
2020-05-09 19:08:22 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int amount = 3518;
+        int amount = 1000;
         int miles = amount / 20;
 
         System.out.println("Начислено миль: " + miles);
